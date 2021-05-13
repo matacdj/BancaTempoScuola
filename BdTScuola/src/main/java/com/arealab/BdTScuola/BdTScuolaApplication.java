d37396e0b3b86029fa588f5f05235b14c723d799
Matteo Accordini <matteoaccordini18@gmail.com>
2021-05-13 09:56:25 +0200

    Creazione progetto vuoto BdTScuola

---
+++ BdTScuola/src/main/java/com/arealab/BdTScuola/BdTScuolaApplication.java
@@ -0,0 +1,13 @@
+package com.arealab.BdTScuola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdTScuolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdTScuolaApplication.class, args);
+	}
+
+}
---
+++ BdTScuola/src/test/java/com/arealab/BdTScuola/BdTScuolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arealab.BdTScuola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdTScuolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
