251eefa83e8a849b1225c50c506bf76545346c65
Matteo Accordini <matteoaccordini18@gmail.com>
2021-05-27 09:31:52 +0200

    Update pom.xml, BdTS.java, and 3 more files...

---
+++ BdTScuola/src/main/java/com/arealab/BdTScuola/bdts/BdTS.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.arealab.BdTScuola.bdts;
+
+import com.arealab.BdTScuola.entity.Docente;
+
+/**
+ *
+ * @author Pingu
+ */
+public class BdTS {
+    
+}
